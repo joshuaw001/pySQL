c9f1ca7533c15e1d10031db6fa9905282e2e1ca9
joshuaw001 <47309531+joshuaw001@users.noreply.github.com>
2019-07-09 13:58:33 -0500

    Update dir_build.py

--- source/dir_build.py
+++ source/dir_build.py
@@ -1,1 +1,17 @@
-
+import pathlib
+import math
+from pathlib import Path
+"""
+basic data storage structure
+c:/-->
+-pySQL
+--db
+---dbname#1
+----name.dbf
+----tblname#1
+-----name.tbl
+-----name.intf
+"""
+with Path("c:/pySQL") as sysdir:
+	if not sysdir.exists():
+		
