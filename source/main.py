8977d3222b4befbe3abf97e80da39a611fefbeeb
joshuaw001 <47309531+joshuaw001@users.noreply.github.com>
2019-07-20 15:21:20 -0500

    Update main.py

--- source/main.py
+++ source/main.py
@@ -1,4 +1,16 @@
 from guizero import App, TextBox
+import math
+
+class db:
+	def __init__(self):
+		self.password = “8888”
+		self.username = “Sql_undefinedUser”
+		self.server   = “localhost”
+		self.name     = “sample_db”
+                
+	def connect(username,password,name):
+                if self.password == password:
+                        
 
 app = App()
 Query = TextBox()
