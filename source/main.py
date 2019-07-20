6f5762a64154f5d06dc8c6c7bd952ec611909ee8
joshuaw001 <47309531+joshuaw001@users.noreply.github.com>
2019-07-20 15:31:32 -0500

    Update main.py

--- source/main.py
+++ source/main.py
@@ -2,15 +2,38 @@ from guizero import App, TextBox
 import math
 
 class db:
-	def __init__(self):
-		self.password = “8888”
-		self.username = “Sql_undefinedUser”
-		self.server   = “localhost”
-		self.name     = “sample_db”
+	_password = “8888”
+	_username = “Sql_undefinedUser”
+	_server   = “localhost”
+	_name     = “sample_db”
                 
 	def connect(username,password,name):
-                if self.password == password:
-                        
+                if _password == password:
+                        dbf.encode
+class dbf:
+	
+
+import pathlib
+import math
+from pathlib import Path
+"""
+basic data storage structure
+c:/-->
+-pySQL
+--db
+---dbname#1
+----name.dbf
+----tblname#1
+-----name.tbl
+-----name.intf
+"""
+
+def buildDir():
+    pass
+
+with open(“c:/pysql”) as sysdir:
+	if not sysdir.exists():
+                pass
 
 app = App()
 Query = TextBox()
