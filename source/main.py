b615abee0344ce6724c89aeaed4fb4515b8b2049
joshuaw001 <47309531+joshuaw001@users.noreply.github.com>
2019-08-06 17:48:32 -0500

    Update main.py

--- source/main.py
+++ source/main.py
@@ -19,37 +19,42 @@ class db:
 class db_files:
 	class dir:
 		curr_dir = os.getcwd()
-	
+		
+		def letter_to_number(letters):   
+    			letters = letters.lower()
+    			dictionary = {'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7,'h':8,'i':9,'j':10,'k':11,'l':12,'m':13,'n':14,'o':15,'p':16,'q':17,'r':18,'s':19,'t':20,'u':21,'v':22,'w':23,'x':24,'y':25,'z':26}
+    			strlen = len(letters)
+    			number = dictionary[letters]
+    			return number
+		
 	class dbf:
-		pass
+		def save(code,path):
+			with open(path,"a") as file:
+        		for i in code:
+           		 _code = db_files.dir.letter_to_number(code)
+            	file.write(_code)
+		return True
+	
+		def load(path):
+    			with open(path,"r") as file:
+        			for i in file.read():
+           				return i
 	
 	class tbl:
-		pass
+		def save(code,path):
+    		with open(path,"a") as file:
+        	for i in code:
+        		_code = db_files.dir.letter_to_number(code)
+        		file.write(_code)
+        return True
+	
+		def load(path):
+    			with open(path,"r") as file:
+        			for i in file.read():
+           				return i
 	
 	class intf:
 		pass
-	
-	def letter_to_number(letters):   
-    		letters = letters.lower()
-    		dictionary = {'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7,'h':8,'i':9,'j':10,'k':11,'l':12,'m':13,'n':14,'o':15,'p':16,'q':17,'r':18,'s':19,'t':20,'u':21,'v':22,'w':23,'x':24,'y':25,'z':26}
-    		strlen = len(letters)
-    		number = dictionary[letters]
-    		return number
-
-	def save(code,path):
-    		with open(path,’a’) as file:
-        	for i in code:
-            		_code = letter_to_number(code)
-            		_code = hex(_code)
-            		file.write(_code)
-            	return True
-	
-	def load(path):
-    		with open(path,’r’) as file:
-        		for i in file.read():
-           		return i
-	
-
 """
 basic data storage structure
 c:/-->
@@ -65,7 +70,7 @@ c:/-->
 def buildDir():
     pass
 
-with open(“c:/pysql”) as sysdir:
+with open("c:/pysql") as sysdir:
 	if not sysdir.exists():
                 pass
 
