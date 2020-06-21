c2465cf7aa8a44e60f5a792232095bedea0aaa1b
joshuaw001 <47309531+joshuaw001@users.noreply.github.com>
2020-06-21 11:33:49 -0500

    created parent-class "DataType(name, value)"

--- source/class files/datatype.py
+++ source/class files/datatype.py
@@ -1,2 +1,9 @@
+import sqlite3
 class DataType:
-    
+    def __init__(self, name, value):
+        self.name  = name
+        self.value = value
+        self.VARDB = {CONN:sqlite3.connect("variables.db")}
+        self.VARDB.update({CURSOR:CONN.cursor()})
+                          
+        self.ID    = 0 #fix this later
