54db3154834ee89066ee71c9db20293864dced1f
joshuaw001 <47309531+joshuaw001@users.noreply.github.com>
2020-06-22 08:44:17 -0500

    added to DataType class

--- source/class files/datatype.py
+++ source/class files/datatype.py
@@ -1,9 +1,16 @@
 import sqlite3
+import 
 class DataType:
-    def __init__(self, name, value):
-        self.name  = name
-        self.value = value
-        self.VARDB = {CONN:sqlite3.connect("variables.db")}
+    def __init__(self, name, value, is_active=True):
+        self.name    = name
+        self.value   = value
+        self.vartype = None
+        self.varstate = is_active
+        self.VARDB   = {CONN:sqlite3.connect("variables.db")}
         self.VARDB.update({CURSOR:CONN.cursor()})
                           
         self.ID    = 0 #fix this later
+    def __str__(self):
+        return f"{self.name}, no type, set to {self.value}."
+    def __del__(self):
+        return f"deleted variable @ {self.ID}."
