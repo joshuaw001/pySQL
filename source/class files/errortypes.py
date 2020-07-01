a83ee7bed10500c062d59aeb46456189217ce040
joshuaw001 <47309531+joshuaw001@users.noreply.github.com>
2020-07-01 10:58:02 -0500

    Update errortypes.py

--- source/class files/errortypes.py
+++ source/class files/errortypes.py
@@ -1,2 +1,7 @@
+import sqlite3
 class PYSQLError:
-    pass
+    def __init__(self):
+        #self.ERROR_ID = 0 #deal with this later
+        self.ERROR_TEXT = f"{self.__name__}: Error type can't be specified at this time."
+        #insert the database insert
+        #code for errors.db here...
