92cfee90d802da7456bfea071386b3e5ee0bf57d
joshuaw001 <47309531+joshuaw001@users.noreply.github.com>
2020-07-01 11:13:00 -0500

    Update errortypes.py

--- source/class files/errortypes.py
+++ source/class files/errortypes.py
@@ -5,3 +5,9 @@ class PYSQLError:
         self.ERROR_TEXT = f"{self.__name__}: Error type can't be specified at this time."
         #insert the database insert
         #code for errors.db here...
+
+class VariableNotActiveError(PYSQLError):
+    def __init__(self,varname):
+        super().__init__()
+        self.ERROR_TEXT = f"{self.__name__}: the variable definition {varname} is in an inactive state. re-activation is required for further use of {varname}."
+        self.varname = varname
