b62768867d7c850d3131e0ad986645ca9a692218
joshuaw001 <47309531+joshuaw001@users.noreply.github.com>
2020-07-01 10:26:12 -0500

    Update datatype.py

--- source/class files/datatype.py
+++ source/class files/datatype.py
@@ -1,9 +1,10 @@
 import sqlite3
-import 
+import errortypes
 class DataType:
     def __init__(self, name, value, is_active=True):
         self.name    = name
         self.value   = value
+        self.valuecache = []
         self.vartype = None
         self.varstate = is_active
         self.VARDB   = {CONN:sqlite3.connect("variables.db")}
@@ -13,4 +14,10 @@ class DataType:
     def __str__(self):
         return f"{self.name}, no type, set to {self.value}."
     def __del__(self):
-        return f"deleted variable @ {self.ID}."
+        return f"deleted variable @ {self.ID}. "
+    def redefine(self, newvalue):
+        if self.varstate is True:
+            self.valuecache += self.value
+            self.value = newvalue
+        else:
+            return errortypes.VariableNotActiveError()
